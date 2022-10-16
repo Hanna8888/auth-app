d751bf2c2c819d544bec519f9f1ab77b0cd19a6c
Anna <alexanet855@gmail.com>
2022-10-17 00:46:10 +0300

    add
    add

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import {Auth0Provider} from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
         <Auth0Provider
-            domain="dev-8yrpla1b.eu.auth0.com"
-            clientId="hrZijwntbyWTUL0kx0AsxQnC0oNaYGBk"
-            redirectUri={window.location.origin}
+            domain="dev-3gk1lddq.eu.auth0.com"
+            clientId="pJ1Hx6rTBOlseZ8oEIl90J2qyYj0hi4S"
+            redirectUri="https://hanna8888.github.io/auth-app/"
         >
             <App/>
         </Auth0Provider>
